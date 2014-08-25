2a927d926dd8baa7b0a2e5745dbbae5e580d922c
Kou AMANO <amano.au1@gmail.com>
2014-08-25 17:54:37 +0900

    add

--- SCRIPTS/ConnectivityClass.m
+++ SCRIPTS/ConnectivityClass.m
@@ -32,6 +32,23 @@ C : クラスタ、K : クラスタ数"
 cGDunn::usage = "cGDunn = Min[s, 1, K](Min[t,1,K ; t!=s]( Gd(C[s],C[t])/Max[k,1,K](Gdelta(C[k])) )) .
 Gdelta(S) = 2 (Sum[x\[Element]S](dshort(x,z[S]))) / Length(S) ; z[S] : medoid of S ; S : クラスタ .
 Gd(S,T) = 1 / (Length(S) Length(T)) Sum[x\[Element]S,y\[Element]T](dshort(x,y)) : S,T : クラスタ ."
+(**cPS**)
+cPS::usage = "cPS=1/K \!\(
+\*UnderoverscriptBox[\(\[Sum]\), \(i = 1\), \(K\)]\(
+\*FractionBox[\(1\), \(n[i]\)] \(
+\*UnderscriptBox[\(\[Sum]\), \(x \[Element] S[i]\)]
+\*FractionBox[\(dshort \((x, z[i])\)\), \(dmin\)]\)\)\)
+dmin = Min[n,1,K; m,1,K; n!=m](dshort(z[n],z[m])) ; z[n]: medoid of cluster n , n[i]: num of members of cluster S[i], K: num of clusters, S[i]: cluster S[i]. "
+(**cI**)
+cI::usage = "cI = 1/K* 1/e[K] * D[K]
+e[K] = sum[ i,1,K ]( lE[i] )
+lE[i] = sum[ j,1,n[i] ]( dshort(x[i][j],z[i]) )
+D[K] = max[ i,1,K ; j,1,k ]( dshort(z[i],z[j]) )
+where
+n[i]: num of members of cluster i
+z[i]: medoid of cluster i"
+
+
 
 Begin["`Private`"]
 test[x_] := x^5
@@ -106,6 +123,29 @@ cGDunn[cls_List, dshortMatZeroself_List] :=
       Length[cls]}]];
   subset = Subsets[cls, {2}];
   Min[Map[Gd[#[[1]], #[[2]], dshortMatZeroself] &, subset]]/maxGdelta
+]
+(**cPS**)
+dMin[dshortMatZeroself_, cls_] := Module[
+  {medoids, subdmat},
+  medoids = 
+    Flatten[orgIndexFromMedoidParCL[cls, 
+      findMedoidsParCL[dshortMatZeroself, cls]]];
+  subdmat = subDMat[dshortMatZeroself, medoids];
+  Min[dropDiagonal[subdmat]]
+]
+(**cI**)
+cI[dshortMatZeroself_, cls_] := Module[
+  {size, eK, dK, subdmats, medoidsOfCl, sampleIDsOfmedoids, medDmat},
+  size = Length[cls];
+  subdmats = Map[subDMat[dshortMatZeroself, #] &, cls];
+  medoidsOfCl = Flatten[findMedoidsParCL[dshortMatZeroself, cls]];
+  eK = Tr[
+    Table[Tr[subdmats[[n]][[medoidsOfCl[[n]]]]], {n, 
+      Length[medoidsOfCl]}]];
+  sampleIDsOfmedoids = orgIndexFromMedoidParCL[cls, medoidsOfCl];
+  medDmat = subDMat[dshortMatZeroself, sampleIDsOfmedoids];
+  dK = Max[medDmat];
+  (1/size) (1/eK) dK
 ]
 
 
