209fdcf01ab83229b14314a902d05ff81c26d343
kouamano <amano.au1@gmail.com>
2019-02-06 16:09:29 +0900

    new

---
+++ SCRIPTS/DataFederation.m
@@ -0,0 +1,19 @@
+BeginPackage["DataFederation`"]
+
+ToPartitionSheet::usage =
+  "ToSpreadSheet[_List,___Options]"
+
+Begin["Private`DataFederation`"]
+
+ToPartitionSheet[101,"xlsx"][list_]:= Module[
+  {itemNames,unitNames,values,rl,rls,lines,cols},
+  itemNames = list[[1,1]];
+  unitNames = list[[1,2]];
+  values = Drop[list[[1]],2];
+  rls = Map[  (rl[#][v_] := Rule[ itemNames[[#]], Quantity[ v,unitNames[[#]] ] ])&, Table[i,{i,Length[itemNames]}]  ];
+  {lines, cols} = Dimensions[values];
+  Transpose[Table[rl[i][values[[j, i]]], {i, cols}, {j, lines}]]
+];
+
+End[];
+EndPackage[];
